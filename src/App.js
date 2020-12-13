be9257d2e84ed1fa6ba0fcd5e9dea34bfcc615ec
bharath raj <braj1410@gmail.com>
2020-12-13 12:07:38 +0530

    Initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
 import logo from './logo.svg';
-import './App.css';
+import {React, useState} from 'react'
+import Web3 from 'web3'
+import WalletConnectProvider from "@walletconnect/web3-provider"
 
 function App() {
+
+
+  const [navColour, setNavColour] = useState('white')
+
+  const Styles = {
+    navbar : {
+      backgroundColor : navColour,
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'center'
+    },
+    logo : {
+      maxHeight: 120,
+    },
+    button: {
+      backgroundColor: 'black',
+      border: 'none',
+      height: 48,
+      paddingLeft: 16,
+      paddingRight: 16,
+      borderRadius: 5,
+      color: 'white',
+      cursor: 'pointer'
+    }
+  }
+
+
+  async function loadWeb3(){
+   if(window.ethereum){
+     window.web3 = new Web3(window.ethereum)
+     await window.ethereum.enable()
+   }
+   else if(window.web3)
+   {
+     window.web3 =new Web3(window.web3.currentProvider)
+   }
+   else{
+     window.alert('Please connect with your metamask wallet')
+   }
+  }
+ async function loadconnectwallet()
+ {
+  const provider = new WalletConnectProvider({
+    rpc: {
+      1: "https://mainnet.mycustomnode.com",
+      3: "https://ropsten.mycustomnode.com",
+      100: "https://dai.poa.network",
+      // ...
+    },
+  });
+  
+  //  Enable session (triggers QR Code modal)
+  await provider.enable();
+ } 
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={Styles.navbar}>
+      <img src={logo} style={Styles.logo}/>
+      <button style={Styles.button} onClick={()=> loadWeb3()}>Connect MetaMask</button>
+      <button style={Styles.button} onClick={()=> loadconnectwallet()}>Connect Wallletconnect</button>
     </div>
   );
-}
+
+
+  }
+    
+   
+   
+        
+
+
+ 
+
+
+
+
 
 export default App;
